31b701d200744d6cb8f1481406f76ab0dd6441e4
Reduan4u <reduanulhaque13@gmail.com>
2023-11-27 20:52:25 +0600

    after dashboard routing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { ObjectId } = require('mongodb');
 const { Schema, model } = require('mongoose');
 const port = process.env.PORT || 5000;
@@ -29,18 +30,94 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-
+        //await client.connect();
 
         const deliveryManCollection = client.db("parcelTracker").collection("deliveryMan");
         const parcelCollection = client.db("parcelTracker").collection("parcels");
         const userCollection = client.db("parcelTracker").collection("users");
 
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
+        // middlewares 
+        const verifyToken = (req, res, next) => {
+            // console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        // use verify admin after verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'Admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+        // use verify delivery man after verifyToken
+        const verifyDeliveryMen = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isDeliveryMen = user?.role === 'DeliveryMen';
+            if (!isDeliveryMen) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
 
         // users related api
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
+        });
+
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'Admin';
+            }
+            res.send({ admin });
+        });
+
+        app.get('/users/deliveryMen/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let deliveryMen = false;
+            if (user) {
+                deliveryMen = user?.role === 'DeliveryMen';
+            }
+            res.send({ deliveryMen });
         });
 
         app.post('/users', async (req, res) => {
